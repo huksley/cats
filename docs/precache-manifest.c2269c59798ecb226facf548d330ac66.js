b094a2c4284797e16b896b29386032533ea58be3
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2018-10-26 15:02:34 +0300

    Publish

---
+++ docs/precache-manifest.c2269c59798ecb226facf548d330ac66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f68d1cdcd1547fe3d11ccaf1df116978",
+    "url": "/cats/static/media/logo.f68d1cdc.jpg"
+  },
+  {
+    "revision": "d0def6b3da292b5aa0e8f0de4860aa4e",
+    "url": "/cats/static/media/facts.d0def6b3.jpg"
+  },
+  {
+    "revision": "acb2fd731a6ef0c91ea2",
+    "url": "/cats/static/js/runtime~main.acb2fd73.js"
+  },
+  {
+    "revision": "32fef04d2654946f15c9",
+    "url": "/cats/static/js/main.32fef04d.chunk.js"
+  },
+  {
+    "revision": "81a0f1fe3e58bee0c466",
+    "url": "/cats/static/js/1.81a0f1fe.chunk.js"
+  },
+  {
+    "revision": "32fef04d2654946f15c9",
+    "url": "/cats/static/css/main.71b1057a.chunk.css"
+  },
+  {
+    "revision": "d1d69c72ec816cff36a61a9116999ba4",
+    "url": "/cats/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/cats/precache-manifest.b98f139f95109e0f8bf2fd207cf829b5.js"
+  "/cats/precache-manifest.c2269c59798ecb226facf548d330ac66.js"
 );
 
 workbox.clientsClaim();
